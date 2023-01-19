abc849c995e01754ab1e9b3a784a471953db632d
Vlado Ivankovic <vladoivankovic@gmail.com>
2023-01-19 12:52:58 +0100

    Setting of light and dark

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+//Setting of light and dark
+$time = date("h:i");
+$sunoff = date_sunset(time());
+$sunon = date_sunrise(time());
+if ($time < $sunoff && $time > $sunon){
+  $modtime = "light";
+}
+else {
+  $modtime = "dark";
+}
+ ?>
+ <!doctype html>
+<!--  in data-bs-theme show light or dark mode -->
+<html lang="en" data-bs-theme="<?php echo "$modtime"; ?>">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+  </body>
+</html>
